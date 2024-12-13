fca94a1c62211aab07a5d47ee814afe6e95118f8
bowook <mbosx316@naver.com>
2024-12-13 15:34:39 +0900

    feat(Command) : 기능 선택 ENUM 생성

--- src/main/java/pairmatching/domain/constant/Command.java
+++ src/main/java/pairmatching/domain/constant/Command.java
@@ -1,2 +1,25 @@
-package pairmatching.domain.constant;public class Command {
+package pairmatching.domain.constant;
+
+import java.util.Arrays;
+import pairmatching.exception.CustomException;
+import pairmatching.exception.ErrorMessage;
+
+public enum Command {
+    PAIR_MATCHING("1"),
+    PAIR_SELECT("2"),
+    PAIR_INIT("3"),
+    QUIT("Q");
+
+    private final String name;
+
+    Command(final String name) {
+        this.name = name;
+    }
+
+    public static Command from(final String input) {
+        return Arrays.stream(Command.values())
+                .filter(command -> command.name.equals(input))
+                .findFirst()
+                .orElseThrow(() -> CustomException.from(ErrorMessage.INPUT_FUNC_ERROR));
+    }
 }
