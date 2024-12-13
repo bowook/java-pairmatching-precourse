a7fc78a76ab084845aa1dc64dc0fb7b14bc09327
bowook <mbosx316@naver.com>
2024-12-13 18:14:50 +0900

    feat(OutputView) : 출력 기능 추가

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,2 +1,31 @@
-package pairmatching.view;public class OutputView {
+package pairmatching.view;
+
+import pairmatching.domain.Pairs;
+import pairmatching.domain.constant.Course;
+import pairmatching.domain.constant.Mission;
+
+public class OutputView {
+    private final static String DIVIDE_LINE = "#############################################";
+    private final static String PAIR_MATCHING_RESULT = "페어 매칭 결과입니다.";
+    private final static String INITIALIZE = "초기화 되었습니다.";
+
+    public void writeInitialize() {
+        System.out.println(INITIALIZE);
+    }
+
+    public void writeMatchingResult(Pairs pairs) {
+        System.out.println(PAIR_MATCHING_RESULT);
+        System.out.println(pairs.toString());
+    }
+
+    public void writeErrorMessage(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public void writeInfo() {
+        System.out.println(DIVIDE_LINE);
+        System.out.println(Course.info());
+        System.out.println(Mission.info());
+        System.out.println(DIVIDE_LINE);
+    }
 }
