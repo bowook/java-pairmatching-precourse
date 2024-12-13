16ab5590fcb57d3430d0075bc78b3a8c63d48f33
bowook <mbosx316@naver.com>
2024-12-13 18:11:43 +0900

    feat(History) : History 도메인 추가

--- src/main/java/pairmatching/domain/History.java
+++ src/main/java/pairmatching/domain/History.java
@@ -1,2 +1,63 @@
-package pairmatching.domain;public class History {
+package pairmatching.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import pairmatching.domain.constant.Course;
+import pairmatching.domain.constant.Level;
+import pairmatching.domain.constant.Mission;
+
+public class History {
+    private final List<Pairs> history = new ArrayList<>();
+
+    public Pairs findLastPairs() {
+        return history.get(history.size() - 1);
+    }
+
+    public boolean addPairsToHistory(Pairs pairs) {
+        if (checkDuplicateCourse(pairs)) {
+            history.add(pairs);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkDuplicateCourse(Pairs pairs) {
+        for (Pairs historyPairs : history) {
+            if (historyPairs.getCourse().equals(pairs.getCourse()) && checkDuplicateLevel(historyPairs, pairs)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkDuplicateLevel(Pairs historyPairs, Pairs pairs) {
+        if (historyPairs.getLevel().equals(pairs.getLevel())) {
+            return checkDuplicateMission(historyPairs, pairs);
+        }
+        return false;
+    }
+
+    private boolean checkDuplicateMission(Pairs historyPairs, Pairs pairs) {
+        if (historyPairs.getMission().equals(pairs.getMission())) {
+            return historyPairs.checkDuplicatePairs(pairs);
+        }
+        return false;
+    }
+
+    public Pairs findPairs(Course course, Level level, Mission mission) {
+        List<Pairs> resultPairs = new ArrayList<>();
+        for (Pairs pairs : history) {
+            if (pairs.getCourse().equals(course) && pairs.getLevel().equals(level) && pairs.getMission()
+                    .equals(mission)) {
+                resultPairs.add(pairs);
+            }
+        }
+        return resultPairs.get(resultPairs.size() - 1);
+    }
+
+    public void initialize() {
+        for (Pairs pairs : history) {
+            pairs.initialize();
+        }
+    }
 }
