cee0b566f81927e103f5e8d0a557dd8d1a9b0dad
bowook <mbosx316@naver.com>
2024-12-13 18:12:38 +0900

    feat(Answer) : Answer 도메인 추가

--- src/main/java/pairmatching/domain/constant/Answer.java
+++ src/main/java/pairmatching/domain/constant/Answer.java
@@ -1,2 +1,22 @@
-package pairmatching.domain.constant;public enum Answer {
+package pairmatching.domain.constant;
+
+import java.util.Arrays;
+import pairmatching.exception.CustomException;
+import pairmatching.exception.ErrorMessage;
+
+public enum Answer {
+    YES("네"),
+    NO("아니오");
+    private final String name;
+
+    Answer(final String name) {
+        this.name = name;
+    }
+
+    public static Answer from(String input) {
+        return Arrays.stream(Answer.values())
+                .filter(answer -> answer.name.equals(input))
+                .findFirst()
+                .orElseThrow(() -> CustomException.from(ErrorMessage.YES_OR_NO));
+    }
 }
