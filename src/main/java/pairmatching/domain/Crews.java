5e60cf9da960d8b8f786f75c6558191c91de1950
bowook <mbosx316@naver.com>
2024-12-13 18:11:22 +0900

    feat(Crews) : Crews 도메인 추가

--- src/main/java/pairmatching/domain/Crews.java
+++ src/main/java/pairmatching/domain/Crews.java
@@ -1,2 +1,21 @@
-package pairmatching.domain;public class Crews {
+package pairmatching.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Crews {
+    private final List<Crew> crews = new ArrayList<>();
+
+    public void addCrew(Crew crew) {
+        this.crews.add(crew);
+    }
+
+    public Crew findCrew(String crewName) {
+        for (Crew crew : crews) {
+            if (crew.getName().equals(crewName)) {
+                return crew;
+            }
+        }
+        return null;
+    }
 }
