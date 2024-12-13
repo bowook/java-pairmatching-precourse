3e8b5d28f9973acd96ba737f3557e80c0f9d82be
bowook <mbosx316@naver.com>
2024-12-13 18:14:24 +0900

    feat(Parser) : 파서 기능 추가

--- src/main/java/pairmatching/utils/Parser.java
+++ src/main/java/pairmatching/utils/Parser.java
@@ -1,2 +1,20 @@
-package pairmatching.utils;public class Parser {
+package pairmatching.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import pairmatching.domain.constant.Course;
+import pairmatching.domain.constant.Level;
+import pairmatching.domain.constant.Mission;
+
+public class Parser {
+    private final static String COMMA = ",";
+
+    public static List<String> nameToInfo(String infos) {
+        List<String> info = Arrays.asList(infos.split(COMMA));
+        Course course = Course.from(info.get(0));
+        Level level = Level.from(info.get(1));
+        Mission mission = Mission.from(info.get(2));
+
+        return info;
+    }
 }
