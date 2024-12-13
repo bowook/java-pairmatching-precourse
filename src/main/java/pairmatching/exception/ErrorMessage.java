518cf4827c9c05d74ca2b52aea48d35b15860c8f
bowook <mbosx316@naver.com>
2024-12-13 15:34:15 +0900

    feat(ErrorMessage) : 파일 읽어오기 에러 메시지 추가

--- src/main/java/pairmatching/exception/ErrorMessage.java
+++ src/main/java/pairmatching/exception/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     INPUT_FUNC_ERROR("1,2,3,Q 중 하나의 기능을 입력하세요."),
     INPUT_MISSION_ERROR("해당되는 미션이 없습니다."),
     NO_MATCHING_ERROR("매칭 이력이 없습니다."),
-    CANT_START_MATCHING("매칭을 진행할 수 없습니다.");
+    CANT_START_MATCHING("매칭을 진행할 수 없습니다."),
+    FILE_ERROR("파일이 존재하지 않습니다.");
 
 
     private final String message;
