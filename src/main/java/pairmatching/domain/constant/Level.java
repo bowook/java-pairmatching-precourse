8c72f2c5527a0564c3c80293db954513aaa37bcf
bowook <mbosx316@naver.com>
2024-12-13 18:13:30 +0900

    feat(Level) : Level 도메인 추가

--- src/main/java/pairmatching/domain/constant/Level.java
+++ src/main/java/pairmatching/domain/constant/Level.java
@@ -1,2 +1,30 @@
-package pairmatching.domain.constant;public enum Level {
+package pairmatching.domain.constant;
+
+import java.util.Arrays;
+import pairmatching.exception.CustomException;
+import pairmatching.exception.ErrorMessage;
+
+public enum Level {
+    ONE("레벨1"),
+    TWO("레벨2"),
+    THREE("레벨3"),
+    FOUR("레벨4"),
+    FIVE("레벨5");
+
+    private final String name;
+
+    Level(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Level from(String input) {
+        return Arrays.stream(Level.values())
+                .filter(level -> level.name.equals(input.trim()))
+                .findFirst()
+                .orElseThrow(() -> CustomException.from(ErrorMessage.INPUT_LEVEL_ERROR));
+    }
 }
