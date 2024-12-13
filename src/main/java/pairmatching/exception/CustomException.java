2218fb478feffa04422bcb574462be77b9c1d506
bowook <mbosx316@naver.com>
2024-12-13 15:29:18 +0900

    feat(Exception) : 예외 처리 및 메시지 기능 추가

--- src/main/java/pairmatching/exception/CustomException.java
+++ src/main/java/pairmatching/exception/CustomException.java
@@ -1,2 +1,13 @@
-package pairmatching.exception;public class CustomException {
+package pairmatching.exception;
+
+public class CustomException extends IllegalArgumentException {
+    private static final String PREFIX = "[ERROR] ";
+
+    private CustomException(ErrorMessage errorMessage) {
+        super(PREFIX + errorMessage.getMessage());
+    }
+
+    public static CustomException from(ErrorMessage errorMessage) {
+        return new CustomException(errorMessage);
+    }
 }
--- src/main/java/pairmatching/exception/ErrorMessage.java
+++ src/main/java/pairmatching/exception/ErrorMessage.java
@@ -1,2 +1,20 @@
-package pairmatching.exception;public enum ErrorMessage {
+package pairmatching.exception;
+
+public enum ErrorMessage {
+
+    INPUT_FUNC_ERROR("1,2,3,Q 중 하나의 기능을 입력하세요."),
+    INPUT_MISSION_ERROR("해당되는 미션이 없습니다."),
+    NO_MATCHING_ERROR("매칭 이력이 없습니다."),
+    CANT_START_MATCHING("매칭을 진행할 수 없습니다.");
+
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
 }
